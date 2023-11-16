682de4d0ebcc193a8fd7698af7ab8100b777e9e9
aint9 <55106920+aint9@users.noreply.github.com>
2023-11-17 06:06:04 +1000

    Update handlers.py
    fix for the BytesIO

--- camelot/handlers.py
+++ camelot/handlers.py
@@ -1,24 +1,23 @@
+# -*- coding: utf-8 -*-
+
 import os
 import sys
-from pathlib import Path
-from typing import Union
 
-from pypdf import PdfReader
-from pypdf import PdfWriter
-from pypdf._utils import StrByteType
+from pypdf import PdfReader, PdfWriter
 
 from .core import TableList
-from .parsers import Lattice
-from .parsers import Stream
-from .utils import TemporaryDirectory
-from .utils import download_url
-from .utils import get_page_layout
-from .utils import get_rotation
-from .utils import get_text_objects
-from .utils import is_url
+from .parsers import Stream, Lattice
+from .utils import (
+    TemporaryDirectory,
+    get_page_layout,
+    get_text_objects,
+    get_rotation,
+    is_url,
+    download_url,
+)
 
 
-class PDFHandler:
+class PDFHandler(object):
     """Handles all operations like temp directory creation, splitting
     file into single page PDFs, parsing each PDF and then removing the
     temp directory.
@@ -35,16 +34,13 @@ class PDFHandler:
 
     """
 
-    def __init__(self, filepath: Union[StrByteType, Path], pages="1", password=None):
+    def __init__(self, filepath, pages="1", password=None):
         if is_url(filepath):
             filepath = download_url(filepath)
-        self.filepath: Union[StrByteType, Path] = filepath
-
-        if isinstance(filepath, str) and not filepath.lower().endswith(".pdf"):
-            raise NotImplementedError("File format not supported")
+        self.filepath = filepath
 
         if password is None:
-            self.password = ""  # noqa: S105
+            self.password = ""
         else:
             self.password = password
             if sys.version_info[0] < 3:
@@ -70,32 +66,19 @@ class PDFHandler:
         """
         page_numbers = []
 
-        if pages == "1":
-            page_numbers.append({"start": 1, "end": 1})
-        else:
-            infile = PdfReader(self.filepath, strict=False)
+        infile = PdfReader(self.filepath, strict=False)
 
-            if infile.is_encrypted:
-                infile.decrypt(self.password)
+        if infile.is_encrypted:
+            infile.decrypt(self.password)
 
-            if pages == "all":
-                page_numbers.append({"start": 1, "end": len(infile.pages)})
-            else:
-                for r in pages.split(","):
-                    if "-" in r:
-                        a, b = r.split("-")
-                        if b == "end":
-                            b = len(infile.pages)
-                        page_numbers.append({"start": int(a), "end": int(b)})
-                    else:
-                        page_numbers.append({"start": int(r), "end": int(r)})
+        page_numbers.append({"start": 1, "end": len(infile.pages)})
 
-        result = []
+        P = []
         for p in page_numbers:
-            result.extend(range(p["start"], p["end"] + 1))
-        return sorted(set(result))
+            P.extend(range(p["start"], p["end"] + 1))
+        return sorted(set(P))
 
-    def _save_page(self, filepath: Union[StrByteType, Path], page, temp):
+    def _save_page(self, filepath, page, temp):
         """Saves specified page from PDF into a temporary directory.
 
         Parameters
@@ -108,7 +91,7 @@ class PDFHandler:
             Tmp directory.
 
         """
-        infile = PdfReader(filepath, strict=False)
+        infile = PdfReader(self.filepath, strict=False)
         if infile.is_encrypted:
             infile.decrypt(self.password)
         fpath = os.path.join(temp, f"page-{page}.pdf")
@@ -143,7 +126,7 @@ class PDFHandler:
             instream.close()
 
     def parse(
-        self, flavor="lattice", suppress_stdout=False, layout_kwargs=None, **kwargs
+        self, flavor="lattice", suppress_stdout=False, layout_kwargs={}, **kwargs
     ):
         """Extracts tables by calling parser.get_tables on all single
         page PDFs.
@@ -156,8 +139,7 @@ class PDFHandler:
         suppress_stdout : str (default: False)
             Suppress logs and warnings.
         layout_kwargs : dict, optional (default: {})
-            A dict of `pdfminer.layout.LAParams
-            <https://github.com/euske/pdfminer/blob/master/pdfminer/layout.py#L33>`_ kwargs.
+            A dict of `pdfminer.layout.LAParams <https://github.com/euske/pdfminer/blob/master/pdfminer/layout.py#L33>`_ kwargs.
         kwargs : dict
             See camelot.read_pdf kwargs.
 
@@ -167,9 +149,6 @@ class PDFHandler:
             List of tables found in PDF.
 
         """
-        if layout_kwargs is None:
-            layout_kwargs = {}
-
         tables = []
         with TemporaryDirectory() as tempdir:
             for p in self.pages:
